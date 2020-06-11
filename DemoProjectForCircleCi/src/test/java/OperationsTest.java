292e005e1a9e406c12025d15377606aed8dac3e3
Norbert Kiss <norbi0728@gmail.com>
2020-06-11 15:38:11 +0200

    add modification

--- DemoProjectForCircleCi/src/test/java/OperationsTest.java
+++ DemoProjectForCircleCi/src/test/java/OperationsTest.java
@@ -10,7 +10,7 @@ public class OperationsTest {
         assertEquals(Operations.add(-3,5),2);
         assertEquals(Operations.add(-3,-5),-8);
         assertEquals(Operations.add(3,0),3);
-        assertEquals(Operations.add(0,-3),-4);
+        assertEquals(Operations.add(0,-3),-3);
     }
 
     @Test
